ac4453a89f596889bfd3346b2241a541e66bb0ea
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-09 08:22:34 +0200

    setting application properties

--- src/main/java/com/example/tp_validation_3/TpValidation3Application.java
+++ src/main/java/com/example/tp_validation_3/TpValidation3Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TpValidation3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TpValidation3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TpValidation3Application.class, args);
+    }
 
 }
