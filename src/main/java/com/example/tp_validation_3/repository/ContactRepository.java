a3d333356d2562d872a8dff1bc6465aae5103e50
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-09 14:17:57 +0200

    modified repository contact

--- src/main/java/com/example/tp_validation_3/repository/ContactRepository.java
+++ src/main/java/com/example/tp_validation_3/repository/ContactRepository.java
@@ -9,5 +9,7 @@ import java.util.Optional;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact,Long> {
     Contact findByEmail(String email);
+    Contact findByLastName(String lastName);
     Optional<Contact> findById(Long id);
+
 }
