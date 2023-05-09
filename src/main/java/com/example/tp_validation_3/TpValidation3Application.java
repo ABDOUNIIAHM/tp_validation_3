a2e8c82fe2aca0a2985e9d2e36a186ee55983d9a
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-09 08:14:22 +0200

    creating repository

---
+++ src/main/java/com/example/tp_validation_3/TpValidation3Application.java
@@ -0,0 +1,13 @@
+package com.example.tp_validation_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpValidation3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpValidation3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tp_validation_3/TpValidation3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_validation_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpValidation3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
