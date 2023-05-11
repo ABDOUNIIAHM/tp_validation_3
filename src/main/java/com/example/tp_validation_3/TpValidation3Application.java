ca7af7f8fb71e83122594360c97859e7ec9b171a
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-11 12:24:39 +0200

    added commandLineRunner

--- src/main/java/com/example/tp_validation_3/TpValidation3Application.java
+++ src/main/java/com/example/tp_validation_3/TpValidation3Application.java
@@ -1,10 +1,38 @@
 package com.example.tp_validation_3;
+import com.example.tp_validation_3.entity.Role;
+import com.example.tp_validation_3.entity.User;
+import com.example.tp_validation_3.repository.RoleRepository;
+import com.example.tp_validation_3.service.IntUserService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class TpValidation3Application {
+public class TpValidation3Application implements CommandLineRunner {
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    IntUserService userService;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
     public static void main(String[] args) {
         SpringApplication.run(TpValidation3Application.class, args);
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+
+
+        userService
+                .findById(6)
+                .setPassword(bCryptPasswordEncoder.encode("admin"));
+        userService
+                .findById(7)
+                .setPassword(bCryptPasswordEncoder.encode("user"));
+
     }
 }
