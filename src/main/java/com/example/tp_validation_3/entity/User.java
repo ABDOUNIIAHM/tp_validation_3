915e32970710df8102dae44e935d406786c41c18
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-11 10:29:11 +0200

    user with role entity updated

--- src/main/java/com/example/tp_validation_3/entity/User.java
+++ src/main/java/com/example/tp_validation_3/entity/User.java
@@ -1,22 +1,65 @@
 package com.example.tp_validation_3.entity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter @Setter @ToString
-@NoArgsConstructor @AllArgsConstructor
-public class User {
+@NoArgsConstructor @AllArgsConstructor @RequiredArgsConstructor
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NonNull
     private String firstName;
+    @NonNull
     private String lastName;
     private String email;
     private String password;
     private String photo;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     private List<Contact> contacts = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    private List<Role> roleList = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roleList
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
